ad2ebbae17a038bea25ef2285a1276e0d5bf0d53
Corey Hastings <47801434+StingzLD@users.noreply.github.com>
2019-12-19 09:26:37 -0600

    Add files via upload

---
+++ IIS/Get-Cert-Put-Into-Variable.ps1
@@ -0,0 +1,16 @@
+#View Currently Installed Cert Hashes
+dir Cert:\LocalMachine\My
+
+
+$certPath = "Cert:\LocalMachine\My"
+$certName = "<cert name>"
+
+#Put First Hash into Variable
+$cert = (Get-ChildItem $certPath | Where-Object { $_.Subject -like "*$certName*" } | Select-Object -First 1).Thumbprint
+
+#Put Second+ Hash into Variable (change "Skip" parameter to select appropriate hash)
+#$cert = (Get-ChildItem $certPath | Where-Object { $_.Subject -like "*$certName*" } | Select-Object -Skip 1).Thumbprint
+
+#Put Last Hash into Variable
+#$cert = (Get-ChildItem $certPath | Where-Object { $_.Subject -like "*$certName*" } | Select-Object -Last 1).Thumbprint
+
