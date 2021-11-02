8abf33cc9fc307829424b28b0a7ec4b4f49a4013
Corey Hastings <stingzld@gmail.com>
2021-11-02 15:43:26 -0500

    updated formatting

--- IIS/Gather-List-of-IIS-Servers-in-Domain.ps1
+++ IIS/Gather-List-of-IIS-Servers-in-Domain.ps1
@@ -1,4 +1,5 @@
 Import-Module Find-WebServerAsync
-$webServers = Get-ADComputer -Filter {enabled -eq $true} -Properties Name | Find-WebServerAsync -Quiet | where {$_.Success -eq $true}
+$webServers = Get-ADComputer -Filter {enabled -eq $true} -Properties Name |
+        Find-WebServerAsync -Quiet | Where-Object {$_.Success -eq $true}
 $webServers | Export-Csv C:\WebServers.csv
 $webServers
