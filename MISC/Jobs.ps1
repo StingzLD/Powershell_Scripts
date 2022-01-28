4b61c3fabf81cbf0996095dd8b44165723f0fe34
Corey Hastings <stingzld@gmail.com>
2022-01-27 20:23:17 -0600

    Completed script for parallel jobs

--- MISC/Jobs.ps1
+++ MISC/Jobs.ps1
@@ -1,33 +1,60 @@
-$scriptBlock = {
-    $true
-}
+[System.Collections.ArrayList]$Global:list = @(
+    'Target0','Target1','Target2','Target3','Target4','Target5'
+)
 
 $jobs = @{}
-$MaxConcurrent = 5
-
-$list = $('Target0','Target1','Target2','Target3','Target4','Target5')
+$Global:maxConcurrent = 5
+$Global:activeJobs = 0
+$listCount = $list.Count
+$Global:targetsProcessed = 0
 
 
 function StartNewJob
 {
-    $job = Start-Job -Name $target -ScriptBlock $scriptBlock
-    $jobs[$target] = New-Object PSObject -Property @{
+    param(
+        [Parameter(Mandatory = $true)]
+        [String]
+        $Name
+    )
+
+    $jobsCount = $jobs.Count
+
+    $job = Start-Job -Name $Name -ScriptBlock $scriptBlock
+    $jobs[$jobsCount] = New-Object PSObject -Property @{
+        Name = $Name
         Job = $job
+        Processed = $false
         Result = $null
     }
+
+#    Start-Sleep -Milliseconds 100
+    $Global:activeJobs += 1
+    Get-Job -Name $Name
 }
 
 
 function CheckForJobs
 {
-    $i = -1
-
-    foreach ($target in $list)
+    if ($Global:list.Count -gt 0)
     {
-        ++$i
-        if ($i -lt $MaxConcurrent)
+        $toRemove = @()
+
+        foreach ($target in $Global:list)
         {
-            StartNewJob
+            if ($Global:activeJobs -lt $Global:maxConcurrent)
+            {
+                StartNewJob -Name $target
+                $toRemove += $target
+            }
+            else
+            {
+                break
+            }
+        }
+
+        foreach ($target in $toRemove)
+        {
+            $Global:list.Remove($target)
         }
     }
 }
@@ -35,15 +62,42 @@ function CheckForJobs
 
 function ProcessJobs
 {
-    if ($i -ge $MaxConcurrent)
+    $jobsCount = $jobs.Count
+
+    for ($i=0; $i -lt $jobsCount; ++$i)
     {
+        if ($jobs[$i].Processed -eq $false)
+        {
+            if ($jobs[$i].Job.State -eq 'Completed' -and $jobs[$i].Job.HasMoreData -eq $true)
+            {
+                $jobs[$i].Result = $jobs[$i].Job.HasMoreData
+                $jobs[$i].Processed = $true
+                $Global:activeJobs -= 1
+                $Global:targetsProcessed += 1
 
+                Start-Sleep -Milliseconds 100
+                Remove-Job -Name $jobs[$i].Name
+            }
+        }
     }
+    Start-Sleep -Milliseconds 2000
 }
 
 
-while ($null -ne $list[0])
+$scriptBlock = {
+    $true
+}
+
+
+while ($Global:targetsProcessed -lt $listCount)
 {
+    Write-Host "Checking for new jobs" -ForegroundColor Cyan
     CheckForJobs
+    Write-Host "Current Active Jobs: $Global:activeJobs `n"
+
+    Write-Host "Proccessing active jobs" -ForegroundColor Cyan
     ProcessJobs
+    Write-Host "$Global:targetsProcessed of $listCount jobs processed `n"
 }
+
+$jobs | Format-List
