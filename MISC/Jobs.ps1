ea874d61da03d73f38227f339a8c3c79c05b5fd9
Corey Hastings <stingzld@gmail.com>
2022-02-03 17:14:32 -0600

    Removed global variables, introduced refs, and added comments

--- MISC/Jobs.ps1
+++ MISC/Jobs.ps1
@@ -1,12 +1,12 @@
-[System.Collections.ArrayList]$Global:list = @(
-    'Target0','Target1','Target2','Target3','Target4','Target5'
+[System.Collections.ArrayList]$list = @(
+    'Target0','Target1','Target2','Target3','Target4','Target5','Target6','Target7','Target8','Target9','Target10'
 )
 
 $jobs = @{}
-$Global:maxConcurrent = 5
-$Global:activeJobs = 0
+$MaxConcurrent = 5
+$activeJobs = 0
 $listCount = $list.Count
-$Global:targetsProcessed = 0
+$targetsProcessed = 0
 
 
 function StartNewJob
@@ -17,9 +17,13 @@ function StartNewJob
         $Name
     )
 
+    # Count the current number of jobs in $jobs
     $jobsCount = $jobs.Count
 
+    # Create a new job
     $job = Start-Job -Name $Name -ScriptBlock $scriptBlock
+
+    # Create a new entry in the $jobs array
     $jobs[$jobsCount] = New-Object PSObject -Property @{
         Name = $Name
         Job = $job
@@ -27,59 +31,75 @@ function StartNewJob
         Result = $null
     }
 
-#    Start-Sleep -Milliseconds 100
-    $Global:activeJobs += 1
-    Get-Job -Name $Name
+    # Get the job's information
+    Write-Host "Created job: " -NoNewLine
+    Write-Host "$Name" -ForegroundColor Yellow
 }
 
 
-function CheckForJobs
+function CheckForJobs ($actJobs)
 {
-    if ($Global:list.Count -gt 0)
+    # If there are still items in $list
+    if ($list.Count -gt 0)
     {
+        Write-Host "Checking for new jobs" -ForegroundColor Cyan
+
+        # Create blank array to store items needing to be removed from list
         $toRemove = @()
 
-        foreach ($target in $Global:list)
+        # Iterate through list
+        foreach ($target in $list)
         {
-            if ($Global:activeJobs -lt $Global:maxConcurrent)
+            # Check if the max number of concurrent jobs running has been met
+            if ($actJobs.Value -lt $MaxConcurrent)
             {
-                StartNewJob -Name $target
-                $toRemove += $target
+                StartNewJob -Name $target  # If not, create a new job for the next item in the list
+                $actJobs.Value += 1  # Increase the number of active jobs running by one
+                $toRemove += $target  # Stage the item from the list for removal
             }
             else
             {
+                # If so, stop iterating through the list
                 break
             }
         }
 
+        # Remove all items in $toRemove from $list
         foreach ($target in $toRemove)
         {
-            $Global:list.Remove($target)
+            $list.Remove($target)
         }
     }
 }
 
 
-function ProcessJobs
+function ProcessJobs ($actJobs, $targsProcessed)
 {
+    Write-Host "Proccessing active jobs" -ForegroundColor Cyan
+
+    # Count the current number of jobs in $jobs
     $jobsCount = $jobs.Count
 
+    # Iterate through $jobs array
     for ($i=0; $i -lt $jobsCount; ++$i)
     {
+        # If the job has not been processed
         if ($jobs[$i].Processed -eq $false)
         {
+            # If the job has completed contains result data to retrieve
             if ($jobs[$i].Job.State -eq 'Completed' -and
                     $jobs[$i].Job.HasMoreData -eq $true)
             {
-                $jobs[$i].Result = $jobs[$i].Job.HasMoreData
-                $jobs[$i].Processed = $true
-                $Global:activeJobs -= 1
-                $Global:targetsProcessed += 1
-                Remove-Job -Name $jobs[$i].Name
+                $jobs[$i].Result = $jobs[$i].Job.HasMoreData  # Add the result data it $job array data
+                $jobs[$i].Processed = $true  # Mark the job as processed
+                $actJobs.Value -= 1  # Decrease the number of active jobs running by one
+                $targsProcessed.Value += 1  # Increase the number of jobs that have been processed by one
+                Remove-Job -Name $jobs[$i].Name  # Remove the job from the system (not the $job array)
             }
         }
     }
-    Start-Sleep -Milliseconds 500
+    # Prevents the flooding of the console
+    Start-Sleep -Milliseconds 1000
 }
 
 
@@ -88,15 +108,13 @@ $scriptBlock = {
 }
 
 
-while ($Global:targetsProcessed -lt $listCount)
+while ($targetsProcessed -lt $listCount)
 {
-    Write-Host "Checking for new jobs" -ForegroundColor Cyan
-    CheckForJobs
-    Write-Host "Current Active Jobs: $Global:activeJobs"
+    CheckForJobs -actJobs ([REF]$activeJobs)
+    ProcessJobs -actJobs ([REF]$activeJobs) -targsProcessed ([REF]$targetsProcessed)
 
-    Write-Host "Proccessing active jobs" -ForegroundColor Cyan
-    ProcessJobs
-    Write-Host "$Global:targetsProcessed of $listCount jobs processed `n"
+#    Write-Host "Current Active Jobs: $activeJobs"
+    Write-Host "$targetsProcessed of $listCount jobs processed `n"
 }
 
-$jobs | Format-List
+$jobs | Format-Table
